b9d277a1da8cb26c67b1d53801d053e1b7a36a3b
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-07-25 00:48:38 -0300

    praticando herença e modificação de acesso

--- banco/banco.php
+++ banco/banco.php
@@ -3,8 +3,10 @@
 require_once 'src/Conta.php';
 require_once 'src/Titular.php';
 require_once 'src/CPF.php';
+require_once 'src/Endereco.php';
 
-$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias');
+$endereco = new Endereco('Rio de Janeiro', 'Centro', 'avenida Presidente Vargas', '233');
+$vinicius = new Titular(new CPF('123.456.789-10'), 'Vinicius Dias', $endereco);
 $primeiraConta = new Conta($vinicius);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
@@ -13,10 +15,13 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia');
+$patricia = new Titular(new CPF('698.549.548-10'), 'Patricia', $endereco);
 $segundaConta = new Conta($patricia);
-var_dump($segundaConta);
+print_r($primeiraConta);
+print_r($segundaConta);
 
-$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg'));
+$outroEndereco = new Endereco('a', 'b', 'c', '1D');
+$outra = new Conta(new Titular(new CPF('123.654.789-01'), 'Abcdefg', $outroEndereco));
+print_r($outra);
 unset($segundaConta);
 echo Conta::recuperaNumeroDeContas();
---
+++ banco/src/Funcionario.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class Funcionario extends Pessoa
+{
+    private $cargo;
+
+    public function __construct(string $nome, CPF $cpf, string $cargo)
+    {
+        parent::__construct($nome, $cpf);
+        $this->cargo = $cargo;
+    }
+
+    public function recuperaCargo(): string
+    {
+        return $this->cargo;
+    }
+
+    public function alteraNome($nome):void
+    {
+        $this->validaNome($nome);
+        $this->nome = $nome;
+    }
+
+
+}
