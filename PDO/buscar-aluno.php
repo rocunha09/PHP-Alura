2a0fe0c7895b0e4f0c3205e179998bfa52b580f4
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-08-05 17:33:55 -0300

    realizado teste com outra forma de busca seja em lista ou unidade através do fetch

--- PDO/buscar-aluno.php
+++ PDO/buscar-aluno.php
@@ -1,1 +1,19 @@
 <?php
+
+require_once 'vendor/autoload.php';
+
+use Alura\Pdo\Domain\Model\Student;
+
+$dbPath = __DIR__.'/banco.sqlite';
+$pdo = new PDO(
+    'sqlite:'.$dbPath
+);
+
+$id = 1;
+
+$statement = $pdo->query('SELECT * FROM students WHERE id = '. $id .';');
+
+$studetnDataList = $statement->fetch(PDO::FETCH_ASSOC);
+
+print_r($studetnDataList);
+
--- PDO/listar-alunos.php
+++ PDO/listar-alunos.php
@@ -11,6 +11,33 @@ $pdo = new PDO(
 
 $statement = $pdo->query('SELECT * FROM students;');
 
+/*
+ * outra forma de realizar a busca de listagem é realizando um fech e tratando item por item
+ * assim se a lista tiver milhares de itens, não irá sobrecarregar a memória...
+ * veja como ficaria logo abaixo:
+ *
+ * obs.: caso queria buscar apenas 1 basta passar o id, caso busque uma lista, ela
+ * estará passando pelo while...
+ *
+ * obs2.: caso seja preciso apenas 1 dado por exemplo, não seria necesário faazer um fetch
+ * ou um fetchAll, bastaria realizarmos um fetchClumn e assim ocupando a memória com menos dados
+ * do que antes.
+ * 
+ */
+/*
+while($studentData = $statement->fetch(PDO::FETCH_ASSOC)){
+    $student = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birthdate'])
+    );
+
+    echo $student->age() . PHP_EOL;
+}
+*/
+
+//abaixo realizando a busca da lista por fechAll
+
 $studetnDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
 $studentList = [];
 
