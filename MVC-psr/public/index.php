037ef42676fb3cdd72c2adde8e8a29b795a17e6a
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-10-15 15:12:27 -0300

    criado novo projeto para dar continuidade ao curso de MVC porém com uso de psr-7 para http-message, etc.

---
+++ MVC-psr/public/index.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Alura\Cursos\Controller\InterfaceControladorRequisicao;
+
+$rotas = require __DIR__ . '/../config/routes.php';
+$caminho = $_SERVER["PATH_INFO"] ?? '/';
+
+//para o script se não receber uma rota válida
+if(!array_key_exists($caminho, $rotas)){
+    //aqui pode-se ter uma página personalizada 404, com um controllador próprio e view
+    //usando http para uma reposta padrão de 404
+    http_response_code(404);
+    exit();
+}
+
+//qualquer controlador irá lidar com a sessão iniciada.
+session_start();
+
+//redireciona caso não esteja logado ou se a rota que esta tentando acessar é diferente de login
+if(!isset($_SESSION['logado']) && stripos($caminho, 'login') === false){
+    header('Location: /login');
+    exit();
+}
+
+
+//caso receba uma rota válida:
+$controllerClass = $rotas[$caminho];
+$controller = new $controllerClass();
+$controller->processaRequisicao();
+
