e151c43a79f83660ea60ed309553e71caa4c6553
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-10-16 13:34:40 -0300

    realizado implementação de psr7 gerando um criador de remensagens http, implementando interface padronizand o uso de request e response na instância e uso dos controllers

--- MVC-psr/public/index.php
+++ MVC-psr/public/index.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Cursos\Controller\InterfaceControladorRequisicao;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
 
 $rotas = require __DIR__ . '/../config/routes.php';
 $caminho = $_SERVER["PATH_INFO"] ?? '/';
@@ -15,17 +17,40 @@ if(!array_key_exists($caminho, $rotas)){
 }
 
 //qualquer controlador irá lidar com a sessão iniciada.
-session_start();
+//session_start();
+//
+////redireciona caso não esteja logado ou se a rota que esta tentando acessar é diferente de login
+//if(!isset($_SESSION['logado']) && stripos($caminho, 'login') === false){
+//    header('Location: /login');
+//    exit();
+//}
 
-//redireciona caso não esteja logado ou se a rota que esta tentando acessar é diferente de login
-if(!isset($_SESSION['logado']) && stripos($caminho, 'login') === false){
-    header('Location: /login');
-    exit();
-}
 
+//aqui está sendo usado um módulo que cria as mensagens, implementando a interface
+//fornecida pela psr-7 aplicada na InterfaceControladorRequisicao
+$psr17Factory = new Psr17Factory();
+
+$creator = new ServerRequestCreator(
+    $psr17Factory, // ServerRequestFactory
+    $psr17Factory, // UriFactory
+    $psr17Factory, // UploadedFileFactory
+    $psr17Factory  // StreamFactory
+);
+
+$request = $creator->fromGlobals();
 
 //caso receba uma rota válida:
 $controllerClass = $rotas[$caminho];
 $controller = new $controllerClass();
-$controller->processaRequisicao();
+$resposta = $controller->processaRequisicao($request);
 
+
+//viasualizado os cabeçalhos da resposta
+foreach ($resposta->getHeaders() as $name =>$values){
+    foreach ($values as $value){
+        header(sprintf('%s : %s', $name, $value), false);
+    }
+}
+
+echo $resposta->getBody();
+exit();
---
+++ MVC-psr/src/Controller/FormularioInsercao.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Alura\Cursos\Controller;
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class FormularioInsercao implements InterfaceControladorRequisicao
+{
+
+    public function processaRequisicao(ServerRequestInterface $request):ResponseInterface
+    {
+        $html = 'Teste';
+        return new Response(200,[],$html);
+    }
+}
