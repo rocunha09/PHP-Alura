e6af8a3ceb6a6efda6320a4a0c08a40159bdef46
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-10-24 07:02:12 -0300

    realizado refatoração em RemovedorDeSerie para que o código fique mais legível.

--- Laravel/series/app/Services/RemovedorDeSerie.php
+++ Laravel/series/app/Services/RemovedorDeSerie.php
@@ -2,32 +2,39 @@
 
 namespace App\Services;
 use Illuminate\Support\Facades\DB;
-use App\{Episodio, serie, Temporada};
+use App\{Episodio, Serie, Temporada};
 
 class RemovedorDeSerie
 {
+    private function removerEp(Temporada $temporada): void
+    {
+        $temporada->episodios->each(function(Episodio $ep){
+            $ep->delete();
+        });
+    }
+
+    private function removerTemp(Serie $serie): void
+    {
+        $serie->temporadas->each(function(Temporada $temporada){
+            $this->removerEp($temporada);
+            $temporada->delete();
+        });
+    }
+
     public function removerSerie(int $idSerie): string
     {
-         //devido ao relacionamento deve-se realizar o destroy em cascata, 
+         //devido ao relacionamento deve-se realizar o destroy em cascata,
         //neste caso será realizado manualmente.
 
-        //primeiro acessar a série, a temporada, apagar os episódios de cada temporada
-        // e remover cada temporada em seguida para então remover a série
         $nomeSerie = '';
         DB::transaction(function() use ($idSerie, &$nomeSerie){
             $serie = Serie::find($idSerie);
             $nomeSerie = $serie->nome;
-            $serie->temporadas->each(function(Temporada $temporada){
-                $temporada->episodios->each(function(Episodio $ep){
-                    $ep->delete();
-                });
-                $temporada->delete();
-            });
-    
-            $serie->delete();       
+            $this->removerTemp($serie);
 
+            $serie->delete();
         });
         return $nomeSerie;
-        
+
     }
 }
