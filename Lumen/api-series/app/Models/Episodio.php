4d2412e0ecede9d1bc3a20f78592f382ad43348d
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-10-31 20:29:30 -0300

    realizando ajuste de retorno do stributo assistido para boolean com accessors do laravel/lumen/elÃoquent

--- Lumen/api-series/app/Models/Episodio.php
+++ Lumen/api-series/app/Models/Episodio.php
@@ -10,10 +10,17 @@ use Illuminate\Http\Request;
 class Episodio extends Model
 {
     public $timestamps = false;
-    protected $fillable = ['temporada', 'numero', 'assistido'];
+    protected $fillable = ['temporada', 'numero', 'assistido', 'serie_id'];
 
     public function serie()
     {
         return $this->belongsTo(Serie::class);
+    }
+
+    //usando accessors do laravel/lumen e fazendo com que o retorno de assistido seja false e não "0"
+    //caso fosse realizar algum setter, ou seja muttator, também seria possível, [ver documentação]
+    public function getAssistidoAttribute($assistido): bool
+    {
+        return $assistido;
     }
 }
