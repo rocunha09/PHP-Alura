0573c7792e305e3127f9768f41b60abd60d4eb81
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-10-31 19:37:45 -0300

    realizado oprefatoração de método get para show, realizado criação do método destroy de forma a retornar mensagens ao não encontrar a série ou mensagem de delete com sucesso, porém o método deleta uma e apenas uma série, o mesmo pode ser refatorado para se tornar din^âmico quanto a quantidade de operações quando for chamado.

--- Lumen/api-series/app/Http/Controllers/SeriesController.php
+++ Lumen/api-series/app/Http/Controllers/SeriesController.php
@@ -15,10 +15,10 @@ class SeriesController
     public function store(Request $request)
     {
         return response()
-            ->json(Serie::create(['nome' => $request->nome]), 201);
+            ->json(Serie::create($request->all()), 201);
     }
 
-    public function get(int $id)
+    public function show(int $id)
     {
         $serie = Serie::find($id);
 
@@ -29,5 +29,45 @@ class SeriesController
 
         return response()
             ->json($serie);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $serie = Serie::find($id);
+        //poderia ser feito desta forma:
+        //$serie->nome = $request->nome;
+
+        if(is_null($serie)){
+            return response()
+                ->json([
+                    'erro' =>'Recurso não encontrado'
+                ], 404);
+        }
+
+        //pode ser feito desta forma, que vai pegar todos dados que vierem da request:
+        $serie->fill($request->all()); //como na classe só  nome é fillable, se vier algo a mais será ignorado.
+        $serie->save();
+
+        return response()
+            ->json($serie);
+    }
+
+    public function destroy(int $id, Request $request)
+    {
+        $serie = Serie::find($id);
+
+        if(is_null($serie)){
+            return response()
+                ->json([
+                    'mensagem' =>'Recurso não encontrado'
+                ], 404);
+        }
+
+        $serie->delete();
+
+        return response()
+            ->json([
+                'mensagem' =>'Serie removida com sucesso!'
+            ], 200);
     }
 }
--- Lumen/api-series/routes/web.php
+++ Lumen/api-series/routes/web.php
@@ -23,6 +23,8 @@ $router->get('/', function () use ($router) {
 $router->group(['prefix'=> '/api'], function() use ($router){
     $router->post('/series', 'SeriesController@store');
     $router->get('/series', 'SeriesController@index');
-    $router->get('/series/{id}', 'SeriesController@get');
+    $router->get('/series/{id}', 'SeriesController@show');
+    $router->put('/series/{id}', 'SeriesController@update');
+    $router->delete('/series/{id}', 'SeriesController@destroy');
 
 });
