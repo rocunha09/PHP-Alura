53562f9410e5a09594b9bfa06f1ea36d0e639aab
Rafael Cunha <51427048+rocunha09@users.noreply.github.com>
2021-10-31 20:08:45 -0300

    refatorando controllers para eliminar duplicação de código, para isso foi utilizado herança criando o BaseController para que Series e EpisodiosController herdem todos métodos já criados.

--- Lumen/api-series/app/Http/Controllers/EpisodiosController.php
+++ Lumen/api-series/app/Http/Controllers/EpisodiosController.php
@@ -3,64 +3,12 @@
 
 namespace App\Http\Controllers;
 use App\Models\Episodio;
-use Illuminate\Http\Request;
 
-class EpisodiosController
+class EpisodiosController extends BaseController
 {
-    public function index()
+    public function __construct()
     {
-        return Episodio::all();
+       $this->classe = Episodio::class;
     }
 
-    public function store(Request $request)
-    {
-        return response()
-            ->json(Episodio::create($request->all()), 201);
-    }
-
-    public function show(int $id)
-    {
-        $serie = Episodio::find($id);
-
-        if(is_null($serie)){
-            return response()
-                ->json('', 204);
-        }
-
-        return response()
-            ->json($serie);
-    }
-
-    public function update(int $id, Request $request)
-    {
-        $serie = Episodio::find($id);
-
-        if(is_null($serie)){
-            return response()
-                ->json([
-                    'erro' =>'Recurso não encontrado'
-                ], 404);
-        }
-
-        $serie->fill($request->all());
-        $serie->save();
-
-        return response()
-            ->json($serie);
-    }
-
-    public function destroy(int $id, Request $request)
-    {
-        $qtdSseriesRemovidas = Episodio::destroy($id);
-
-        if($qtdSseriesRemovidas === 0){
-            return response()
-                ->json([
-                    'erro' =>'Recurso não encontrado'
-                ], 404);
-        }
-
-        return response()
-            ->json('', 204);
-    }
 }
--- Lumen/api-series/app/Http/Controllers/SeriesController.php
+++ Lumen/api-series/app/Http/Controllers/SeriesController.php
@@ -3,73 +3,12 @@
 
 namespace App\Http\Controllers;
 use App\Models\Serie;
-use Illuminate\Http\Request;
 
-class SeriesController
+class SeriesController extends BaseController
 {
-    public function index()
+    public function __construct()
     {
-        return Serie::all();
+        $this->classe = Serie::class;
     }
 
-    public function store(Request $request)
-    {
-        return response()
-            ->json(Serie::create($request->all()), 201);
-    }
-
-    public function show(int $id)
-    {
-        $serie = Serie::find($id);
-
-        if(is_null($serie)){
-            return response()
-                ->json('', 204);
-        }
-
-        return response()
-            ->json($serie);
-    }
-
-    public function update(int $id, Request $request)
-    {
-        $serie = Serie::find($id);
-        //poderia ser feito desta forma:
-        //$serie->nome = $request->nome;
-
-        if(is_null($serie)){
-            return response()
-                ->json([
-                    'erro' =>'Recurso não encontrado'
-                ], 404);
-        }
-
-        //pode ser feito desta forma, que vai pegar todos dados que vierem da request:
-        $serie->fill($request->all()); //como na classe só  nome é fillable, se vier algo a mais será ignorado.
-        $serie->save();
-
-        return response()
-            ->json($serie);
-    }
-
-    public function destroy(int $id, Request $request)
-    {
-        /*
-         * Para remover uma Serie, foi chamado o método estático destroy, que recebe como parâmetro os IDs dos
-         * registros a serem removidos do banco de dados.
-         *
-         * ele retorna o número de registros afetados
-         * */
-        $qtdSseriesRemovidas = Serie::destroy($id);
-
-        if($qtdSseriesRemovidas === 0){
-            return response()
-                ->json([
-                    'erro' =>'Recurso não encontrado'
-                ], 404);
-        }
-
-        return response()
-            ->json('', 204);
-    }
 }
